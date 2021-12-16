0afb11e0680761ae9f14c1f799a84a940b8084e0
Dudko Mikhail <dudkomikhail01@gmail.com>
2021-12-16 10:29:28 +0300

    test

--- lab9/framework/src/test/java/by/bsu/testing/test/TravellingBySearchTest.java
+++ lab9/framework/src/test/java/by/bsu/testing/test/TravellingBySearchTest.java
@@ -4,47 +4,60 @@ import by.bsu.testing.model.TourSearchData;
 import by.bsu.testing.page.TourSearchResultPage;
 import by.bsu.testing.page.TravellingByHomePage;
 import by.bsu.testing.service.TourSearchDataCreator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TravellingBySearchTest extends CommonConditions {
-    private static final TourSearchData searchData = TourSearchDataCreator.withDataFromProperties();
+    private static final Logger logger = LogManager.getLogger();
+    private static TourSearchData searchData;
     private TravellingByHomePage homePage;
+
+    @BeforeClass
+    public void initSearchData() {
+        searchData = TourSearchDataCreator.withDataFromProperties();
+        logger.info(searchData);
+    }
 
     @BeforeMethod
     public void initSearchResultPage() {
         homePage = new TravellingByHomePage(driver);
     }
 
+//    @Test
+//    public void testDestinationCountrySearchField() {
+//        TourSearchResultPage tourSearchResultPage = homePage.selectDestinationCountry(searchData.getDestinationCountry())
+//                        .findTours();
+//        Assert.assertEquals(tourSearchResultPage.getDestinationCountry(), searchData.getDestinationCountry());
+//    }
+//
+//    @Test(priority = 1)
+//    public void testDestinationCitySearchField() {
+//        TourSearchResultPage tourSearchResultPage = homePage.selectDestinationCountry(searchData.getDestinationCountry())
+//                .selectDestinationCity(searchData.getDestinationCity())
+//                .findTours();
+//        Assert.assertEquals(tourSearchResultPage.getDestinationCity(), searchData.getDestinationCity());
+//    }
+//
+//    @Test
+//    public void testDepartureCitySearchField() {
+//        TourSearchResultPage tourSearchResultPage = homePage.selectDepartureCity(searchData.getDepartureCity())
+//                .findTours();
+//        Assert.assertEquals(tourSearchResultPage.getDepartureCity(), searchData.getDepartureCity());
+//    }
+//
+//    @Test(priority = 2)
+//    public void testAllSearchFieldTogether() {
+//        TourSearchResultPage tourSearchResultPage = homePage.searchForTerms(searchData)
+//                .findTours();
+//        Assert.assertTrue(tourSearchResultPage.getDestinationCountry().equals(searchData.getDestinationCountry())
+//                && tourSearchResultPage.getDestinationCity().equals(searchData.getDestinationCity())
+//                && tourSearchResultPage.getDepartureCity().equals(searchData.getDepartureCity()));
+//    }
     @Test
-    public void testDestinationCountrySearchField() {
-        TourSearchResultPage tourSearchResultPage = homePage.selectDestinationCountry(searchData.getDestinationCountry())
-                        .findTours();
-        Assert.assertEquals(tourSearchResultPage.getDestinationCountry(), searchData.getDestinationCountry());
-    }
+    public void nothing() {}
 
-    @Test(priority = 1)
-    public void testDestinationCitySearchField() {
-        TourSearchResultPage tourSearchResultPage = homePage.selectDestinationCountry(searchData.getDestinationCountry())
-                .selectDestinationCity(searchData.getDestinationCity())
-                .findTours();
-        Assert.assertEquals(tourSearchResultPage.getDestinationCity(), searchData.getDestinationCity());
-    }
-
-    @Test
-    public void testDepartureCitySearchField() {
-        TourSearchResultPage tourSearchResultPage = homePage.selectDepartureCity(searchData.getDepartureCity())
-                .findTours();
-        Assert.assertEquals(tourSearchResultPage.getDepartureCity(), searchData.getDepartureCity());
-    }
-
-    @Test(priority = 2)
-    public void testAllSearchFieldTogether() {
-        TourSearchResultPage tourSearchResultPage = homePage.searchForTerms(searchData)
-                .findTours();
-        Assert.assertTrue(tourSearchResultPage.getDestinationCountry().equals(searchData.getDestinationCountry())
-                && tourSearchResultPage.getDestinationCity().equals(searchData.getDestinationCity())
-                && tourSearchResultPage.getDepartureCity().equals(searchData.getDepartureCity()));
-    }
 }
--- lab9/framework/src/test/java/by/bsu/testing/test/TravellingByVisasSearchTest.java
+++ lab9/framework/src/test/java/by/bsu/testing/test/TravellingByVisasSearchTest.java
@@ -3,38 +3,54 @@ package by.bsu.testing.test;
 import by.bsu.testing.model.VisaSearchData;
 import by.bsu.testing.page.SearchAndSelectionVisasPage;
 import by.bsu.testing.page.VisasSearchResultPage;
+import by.bsu.testing.service.TourSearchDataCreator;
 import by.bsu.testing.service.VisaSearchDataCreator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TravellingByVisasSearchTest extends CommonConditions {
-    private static final VisaSearchData searchData = VisaSearchDataCreator.withDataFromProperties();
+    private static final Logger logger = LogManager.getLogger();
+    private static VisaSearchData searchData;
     private SearchAndSelectionVisasPage searchPage;
+
+    @BeforeClass
+    public void initSearchData() {
+        searchData = VisaSearchDataCreator.withDataFromProperties();
+        logger.info(searchData);
+    }
 
     @BeforeMethod
     public void setUpBrowserAndInitSearchVisasPage() {
         searchPage = new SearchAndSelectionVisasPage(driver);
     }
 
-    @Test
-    public void testCountryFilter() {
-        VisasSearchResultPage searchResultPage = searchPage.selectCountry(searchData.getCountry())
-                .findVisas();
-        Assert.assertTrue(searchResultPage.isCountriesCorresponds(searchData.getCountry()));
-    }
+//    @Test
+//    public void testCountryFilter() {
+//        VisasSearchResultPage searchResultPage = searchPage.selectCountry(searchData.getCountry())
+//                .findVisas();
+//        Assert.assertTrue(searchResultPage.isCountriesCorresponds(searchData.getCountry()));
+//    }
+//
+//    @Test
+//    public void testTypeOfEntryFilter() {
+//        VisasSearchResultPage searchResultPage = searchPage.selectTypeOfEntry(searchData.getEntryType())
+//                .findVisas();
+//        Assert.assertTrue(searchResultPage.isTypesOfEntryCorresponds(searchData.getEntryType()));
+//    }
+//
+//    @Test(priority = 1)
+//    public void testAllFiltersTogether() {
+//        VisasSearchResultPage searchResultPage = searchPage.searchForTerms(searchData)
+//                .findVisas();
+//        Assert.assertTrue(searchResultPage.searchDataCorresponds(searchData));
+//    }
 
     @Test
-    public void testTypeOfEntryFilter() {
-        VisasSearchResultPage searchResultPage = searchPage.selectTypeOfEntry(searchData.getEntryType())
-                .findVisas();
-        Assert.assertTrue(searchResultPage.isTypesOfEntryCorresponds(searchData.getEntryType()));
-    }
+    public void nothing() {
 
-    @Test(priority = 1)
-    public void testAllFiltersTogether() {
-        VisasSearchResultPage searchResultPage = searchPage.searchForTerms(searchData)
-                .findVisas();
-        Assert.assertTrue(searchResultPage.searchDataCorresponds(searchData));
     }
 }
